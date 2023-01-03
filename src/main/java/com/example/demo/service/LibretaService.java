c8ca8cca616bc5ea0919757a7c49d4e204ceb50b
dapevi1997 <dapevi97@gmail.com>
2023-01-03 11:20:30 -0500

    Capa de servicios

---
+++ src/main/java/com/example/demo/service/LibretaService.java
@@ -0,0 +1,67 @@
+package com.example.demo.service;
+
+import com.example.demo.service.interfaces.ILibreta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+public class LibretaService implements ILibreta{
+
+/*    @Autowired
+    private ContactoRepository contactoRepository;
+
+    @Autowired
+    private TelefonoRepository telefonoRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Contacto> getList() { }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Contacto> getList(String field, Sort.Direction order) { }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Contacto> searchContacto(String dataToSearch) { }
+
+    @Override
+    @Transactional
+    public Contacto createContacto(Contacto contacto) { }
+
+    @Override
+    @Transactional
+    public Telefono createTelefono(Telefono telefono) { }
+
+    @Override
+    @Transactional
+    public Contacto updateContacto(Integer id, Contacto contacto) { }
+
+    @Override
+    @Transactional
+    public Contacto updateNombre(Integer id, Contacto contacto) { }
+
+    @Override
+    @Transactional
+    public Contacto updateApellidos(Integer id, Contacto contacto) { }
+
+    @Override
+    @Transactional
+    public Telefono updateTelefono(Integer id, Telefono telefono) { }
+
+    @Override
+    @Transactional
+    public Telefono updateOnlyTelefono(Integer id, Telefono telefono) { }
+
+    @Override
+    @Transactional
+    public Contacto deleteContacto(Integer id) { }
+    @Override
+    @Transactional
+    public Telefono deleteTelefono(Integer id) { }*/
+
+
+}
---
+++ src/main/java/com/example/demo/service/interfaces/ILibreta.java
@@ -0,0 +1,16 @@
+package com.example.demo.service.interfaces;
+
+public interface ILibreta {
+/*    public List<Contacto> getList();
+    public List<Contacto> getList(String field, Sort.Direction order);
+    public List<Contacto> searchContacto(String dataToSearch);
+    public Contacto createContacto(Contacto contacto);
+    public Telefono createTelefono(Telefono telefono);
+    public Contacto updateContacto(Integer id, Contacto contacto);
+    public Contacto updateNombre(Integer id, Contacto contacto);
+    public Contacto updateApellidos(Integer id, Contacto contacto);
+    public Telefono updateTelefono(Integer id, Telefono telefono);
+    public Telefono updateOnlyTelefono(Integer id, Telefono telefono);
+    public Contacto deleteContacto(Integer id);
+    public Telefono deleteTelefono(Integer id);*/
+}
